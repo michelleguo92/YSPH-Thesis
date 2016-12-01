28a9b97ff3e3837d1cec2a18a36ec55573984325
michelleguo92 <michelle.guo@yale.edu>
2016-12-01 14:33:43 -0500

    Add school closure function

--- Functions/schoolclosure.R
+++ Functions/schoolclosure.R
@@ -1,30 +1,32 @@
-vaccinate <- function(beta,gamma,nu,S0,I0,R0,V0,tau0,maxtime,nsims,closureeff,closurethresh,closureduration){
-  N <- S0 + I0 + R0 + V0
+schoolclosure <- function(beta,gamma,S0,I0,R0,tau0,maxtime,nsims,closeeff,closurethresh,closureduration){
+  N <- S0 + I0 + R0
   i <- 1 #time step during replication
   j <- 1 #replication index
   #simulate data
   
-  output <- matrix(NA,nrow=maxtime-1,ncol=5)
-  output <- rbind(c(S0,I0,R0,V0,tau0),output)
   sims = list()
   infected <- list()
   nrec <- matrix(data=NA, nrow=nsims, ncol=maxtime)
-  nvax <- matrix(data=NA, nrow=nsims, ncol=maxtime)
-  maxrec <- array(data=NA, dim=nsims)
-  maxvax <- array(data=NA, dim=nsims)
+  maxtau <- matrix(data=NA, nrow=nsims, ncol=maxtime)
   #Run gillespie simulation with above parameters
   
   for (j in 1:nsims) {
+    print(j)
     i = 1
     t = 0
     dt = 0
-    tempclosureeff = 0
+    tempeff = 0
     closed = 0
+    tempduration = 0
+    durationclosed = 0
+    
+    # Moved output initialization here. You were initially setting it elsewhere, but this placement makes more sense
+    output <- matrix(NA,nrow=maxtime-1,ncol=4)
+    output <- rbind(c(S0,I0,R0,tau0),output)
     while (t<maxtime && output[i,2]>0 && output[i,1]>0) { 
-      rho = (beta*output[i,1]*output[i,2])/N #time to next infection
-      mu = (1-tempclosureeff)*gamma*output[i,2]                 #time to next recovery
-      alpha = nu*output[i,1]                 #time to next vaccination 
-      lambda = (rho + mu + alpha)            #UPDATE WHEN YOU ADD NEW COMPARTMENT
+      rho = ((1-tempeff)*(beta*output[i,1]*output[i,2]))/N      #time to next infection
+      mu = gamma*output[i,2]                                    #time to next recovery                
+      lambda = (rho + mu)                                       #UPDATE WHEN YOU ADD NEW COMPARTMENT
       p1 = rho/lambda
       p2 = mu/lambda
       u1 = runif(1)
@@ -34,47 +36,45 @@ vaccinate <- function(beta,gamma,nu,S0,I0,R0,V0,tau0,maxtime,nsims,closureeff,cl
         output[i+1,1] = output[i,1] - 1
         output[i+1,2] = output[i,2] + 1
         output[i+1,3] = output[i,3]
-        output[i+1,4] = output[i,4]
-      } else if (u2 >= p1 && u2 < p1+p2){
+      } else {
         output[i+1,1] = output[i,1]
         output[i+1,2] = output[i,2] - 1
         output[i+1,3] = output[i,3] + 1
-        output[i+1,4] = output[i,4]
-      } else if (u2 >= p1+p2){
-        output[i+1,1] = output[i,1] - 1
-        output[i+1,2] = output[i,2] 
-        output[i+1,3] = output[i,3] 
-        output[i+1,4] = output[i,4] + 1
-      }
+      } 
       t = t + dt
-      output[i+1,5] = t
+      output[i+1,4] = t
       i = i + 1
-      if (closed = 0 && output[i,2] > closurethresh) {
-        ifclosed = 1
-        tempclosureeff = closureeff #update this if count for I exceeds threshold
+      if (closed == 0 && output[i,2] > closurethresh) {
+        closed = 1
+        tempeff = closeeff #update this if count for I exceeds threshold
       }
-      if (closed = 1 && tempduration >= closureduration) {
-        ifclosed = 0
-        tempclosureeff = 0
+      else if (closed == 1 && durationclosed >= closureduration) {
+        closed = 0
+        tempeff = 0
+        durationclosed = 0
+      }
+      if (closed == 1) {
+        durationclosed = durationclosed + 1
       }
     }
+    # Moved sims here so that you didn't have to insert output into sims
     sims[[j]] = output[]
-    output <- matrix(NA,nrow=maxtime-1,ncol=5)
-    output <- rbind(c(S0,I0,R0,V0,tau0),output)
   }
   for (i in 1:nsims) {
-    nrec[i,] = sims[[i]][,3]
+    nrec[i,] = sims[[i]][,3] 
   }
   
   for (i in 1:nsims) {
-    nvax[i,] = sims[[i]][,4]
+    maxtau[i,] = max(sims[[i]][,4],na.rm=TRUE)
   }
-  result <- matrix(NA,nrow=nsims,ncol=2)
-  maxrec <- apply(nrec,1,max,na.rm=TRUE)
-  maxvax <- apply(nvax,1,max,na.rm=TRUE)
-  result[,1] <- maxvax
-  result[,2] <- maxrec
-  #
-  return(result)
   
+  maxt = max(maxtau)
+  
+  plot(1,type="n",xlab="Time",ylab="Total infected",
+       main=NULL,xlim=c(0, 500), 
+       ylim=c(0, 1000))
+  
+  for (i in 1:nsims) {
+    lines(sims[[i]][,4], sims[[i]][,3], col=i)
+  }
 }
